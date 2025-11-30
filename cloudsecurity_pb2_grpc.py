c737e75fb102e9cacb81abf360d7c00fcefab413
FontchaSteve <153062747+FontchaSteve@users.noreply.github.com>
2025-11-30 20:52:05 +0100

    Update cloudsecurity_pb2_grpc.py

--- cloudsecurity_pb2_grpc.py
+++ cloudsecurity_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import cloudsecurity_pb2 as cloudsecurity__pb2
 
-GRPC_GENERATED_VERSION = '1.68.0'
+GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
@@ -18,7 +18,7 @@ except ImportError:
 if _version_not_supported:
     raise RuntimeError(
         f'The grpc package installed is at version {GRPC_VERSION},'
-        + f' but the generated code in cloudsecurity_pb2_grpc.py depends on'
+        + ' but the generated code in cloudsecurity_pb2_grpc.py depends on'
         + f' grpcio>={GRPC_GENERATED_VERSION}.'
         + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
         + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
@@ -39,6 +39,11 @@ class UserServiceStub(object):
                 request_serializer=cloudsecurity__pb2.Request.SerializeToString,
                 response_deserializer=cloudsecurity__pb2.Response.FromString,
                 _registered_method=True)
+        self.verifyOTP = channel.unary_unary(
+                '/cloud.UserService/verifyOTP',
+                request_serializer=cloudsecurity__pb2.OTPRequest.SerializeToString,
+                response_deserializer=cloudsecurity__pb2.Response.FromString,
+                _registered_method=True)
 
 
 class UserServiceServicer(object):
@@ -50,12 +55,23 @@ class UserServiceServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def verifyOTP(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_UserServiceServicer_to_server(servicer, server):
     rpc_method_handlers = {
             'login': grpc.unary_unary_rpc_method_handler(
                     servicer.login,
                     request_deserializer=cloudsecurity__pb2.Request.FromString,
+                    response_serializer=cloudsecurity__pb2.Response.SerializeToString,
+            ),
+            'verifyOTP': grpc.unary_unary_rpc_method_handler(
+                    servicer.verifyOTP,
+                    request_deserializer=cloudsecurity__pb2.OTPRequest.FromString,
                     response_serializer=cloudsecurity__pb2.Response.SerializeToString,
             ),
     }
@@ -85,6 +101,33 @@ class UserService(object):
             target,
             '/cloud.UserService/login',
             cloudsecurity__pb2.Request.SerializeToString,
+            cloudsecurity__pb2.Response.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
+
+    @staticmethod
+    def verifyOTP(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(
+            request,
+            target,
+            '/cloud.UserService/verifyOTP',
+            cloudsecurity__pb2.OTPRequest.SerializeToString,
             cloudsecurity__pb2.Response.FromString,
             options,
             channel_credentials,
